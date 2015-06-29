6d34338af8e85f80daaea4fa8be50b12849bf95e
Gordon Guthrie <gguthrie@basho.com>
2015-06-29 08:38:43 +0000

    update

--- src/ts_runner.erl
+++ src/ts_runner.erl
@@ -1,39 +1,94 @@
 -module(ts_runner).
 
 -export([
-	 put/0,
-	 setup_testing/0
+	 setup/0,
+	 show/0,
+	 load/0,
+	 dump/0,
+	 prep_q/0,
+	 qry/0,
+	 qry/1,
+	 get_ddl/0,
+	 bounce_qry/0
 	]).
 
-%% deprecated
--export([
-	 load/0
-	]).
-
--include("riak_kv_index.hrl").
 -include("riak_kv_ddl.hrl").
+-include_lib("riak_kv/include/riak_kv_index.hrl").
+-include("riak_kv_wm_raw.hrl").
 
--define(BUCKET,   <<"2iBucket">>).
+-define(BUCKET,   <<"TsBucket3">>).
 -define(INDEX,    <<"Index_bin">>).
--define(MD_INDEX, <<"index">>).
 -define(CLIENTID, <<"li_client">>).
+-define(GEOHASH,  "geohash_val").
+-define(USER,     "user_").
 
-put() ->
+bounce_qry() ->
+    exit(whereis(riak_kv_qry_queue), "adios bandito"),
     ok.
 
-setup_testing() ->
+qry() ->
+    Qry = prep_q(),
+    qry(Qry).
+
+qry(#riak_kv_li_index_v1{} = Qry) ->
+    DDL = get_ddl(),
+    io:format("in qry/1~n"),
+    case riak_kv_ddl:is_query_valid(DDL, Qry) of
+	{false, Err} -> exit({invalid_query, Err});
+	true         -> qry2(Qry)
+    end.
+
+qry2(Qry) ->
+    Ret = riak_kv_qry_queue:put_on_queue(Qry),
+    io:format("Ret is ~p~n", [Ret]),
+    ok.
+
+prep_q() ->
+    Selections  = [["temperature"], ["geohash"]],
+    Filters     = [
+		   {'and',
+		    {'=', "geohash", ?GEOHASH},
+		    {'and',
+		     {'>',  "timestamp", 1},
+		     {'=<', "timestamp", 3}
+		    }
+		   }
+		  ],
+    Operators   = [],
+    Sorters     = [],
+    Combinators = [],
+    Limit       = none,
+    _Qry = #riak_kv_li_index_v1{bucket      = ?BUCKET,
+				selections  = Selections,
+				filters     = Filters,
+				operators   = Operators,
+				sorters     = Sorters,
+				combinators = Combinators,
+				limit       = Limit}.
+
+setup() ->
     Client = riak_client:new(node(), ?CLIENTID),
-    DDL = make_ts_testing_ddl(),
-    ok = riak_kv_ddl_compiler:make_validation(DDL, true, "/tmp"),
-    BucketProps = [{ddl, DDL}],
+    DDL = get_ddl(),
+    {module, ModName} = riak_kv_ddl_compiler:make_helper_mod(DDL),
+    BucketProps = [{ddl, 1, DDL}, {verification_module, ModName}],
     ok = riak_client:set_bucket(?BUCKET, BucketProps, Client).
 
-make_ts_testing_ddl() ->
-    Geohash     = {"geohash",     binary},
-    User        = {"user",        binary},
-    Time        = {"time",        timestamp},
-    Weather     = {"weather",     any},
-    Temperature = {"temperature", binary},
+get_ddl() ->
+    Geohash     = #riak_field_v1{name = "geohash",
+				 position = 1,
+				 type     = binary},
+    User        = #riak_field_v1{name     = "user",
+				 position = 2,
+				 type     = binary},
+    Time        = #riak_field_v1{name     = "time",
+				 position = 3,
+				 type     = timestamp},
+    Weather     = #riak_field_v1{name     = "weather",
+				 position = 4,
+				 type     = any},
+    Temperature = #riak_field_v1{name     = "temperature",
+				 position = 5,
+				 type     = binary},
     Fields = [
 	      Geohash,
 	      User,
@@ -41,30 +96,82 @@ make_ts_testing_ddl() ->
 	      Weather,
 	      Temperature
 	     ],
-    Colocation = yando,
-    _DDL = #ddl{bucket     = ?BUCKET,
-		fields     = Fields,
-		colocation = Colocation}.
+    PK_AST = [
+	      #param_v1{name = ["geohash"]},
+	      #hash_fn_v1{mod  = riak_kv_quanta,
+			  fn   = quanta,
+			  args = [#param_v1{name = ["time"]},
+				  15,
+				  m
+				 ]}
+	     ],
+    PK = #partition_key_v1{ast = PK_AST},
+    LK_AST = [
+	      #param_v1{name = ["geohash"]},
+	      #hash_fn_v1{mod  = riak_kv_quanta,
+			  fn   = quanta,
+			  args = [#param_v1{name = ["time"]},
+				  15,
+				  m
+				 ]},
+	      #param_v1{name = ["time"]},
+	      #param_v1{name = ["user"]}
+	     ],
+    LK = #local_key_v1{ast = LK_AST},
+    _DDL = #ddl_v1{bucket        = ?BUCKET,
+		   fields        = Fields,
+		   partition_key = PK,
+		   local_key     = LK}.
 
-%%
-%% obsolete
-%%
-load() -> load(10).
+dump() ->
+    Mod = riak_kv_ddl:make_module_name(?BUCKET),
+    DDL = get_ddl(),
+    {PK, _LK, _Obj} = make_obj(Mod, DDL, 1),
+    leveldb_console:dump_bucket(?BUCKET, PK).
 
-load(0) -> ok;
-load(N) -> RObj = riak_object:new(?BUCKET, make_key(N), make_value(N)),
-	   MD1 = riak_object:get_update_metadata(RObj),
-	   MD2 = dict:store(?MD_INDEX,
-			    [{?INDEX, make_index(N)}],
-			    MD1
-			   ),
-	   gg:format("in load for ~p~n- MD1 is ~p~n- MD2 is ~p~n", [N, dict:to_list(MD1), dict:to_list(MD2)]),
-	   RObj2 = riak_object:update_metadata(RObj, MD2),
-	   riak_client:put(RObj2, {riak_client, [node(), undefined]}).
+load() ->
+    Mod = riak_kv_ddl:make_module_name(?BUCKET),
+    DDL = get_ddl(),
+    load(Mod, DDL, 10).
 
-make_key(N)   -> make_("key", N).
-make_value(N) -> make_("value", N).
+load(_Mod, _DDL, 0) ->
+    ok;
+load(Mod, DDL, N) ->
+    {PK, LK, Obj} = make_obj(Mod, DDL, N),
+    RObj = riak_object:new(?BUCKET, PK, term_to_binary(Obj)),
+    MD1 = riak_object:get_update_metadata(RObj),
+    MD2 = dict:store(?MD_INDEX,
+		     [{?INDEX, make_index(N)}],
+		     MD1
+		    ),
+    MD3 = dict:store(?MD_LI_IDX,
+		     LK,
+		     MD2
+		    ),
+    RObj2 = riak_object:update_metadata(RObj, MD3),
+    riak_client:put(RObj2, {riak_client, [node(), undefined]}),
+    load(Mod, DDL, N - 1).
+
+make_obj(Mod, DDL, N) ->
+    N2 = integer_to_list(N),
+    Geohash = list_to_binary(?GEOHASH),
+    User = list_to_binary(?USER ++ N2),
+    Time = 10000000 + N,
+    Weather = [{some, "random", <<"WeaTH3r">>, N}],
+    Temp = list_to_binary(N2),
+    Obj = {Geohash, User, Time, Weather, Temp},
+    case Mod:validate_obj(Obj) of
+	true  -> PK = riak_kv_ddl:get_partition_key(DDL, Obj),
+		 LK = riak_kv_ddl:get_local_key(DDL, Obj),
+		 {PK, LK, Obj};
+	false -> exit("borked object")
+    end.
+
 make_index(N) -> make_("index", N).
 
 make_(Prefix, N) when is_integer(N) ->
     list_to_binary(Prefix ++ "_" ++ integer_to_list(N)).
+
+show() ->
+    Client = riak_client:new(node(), ?CLIENTID),
+    _Props = riak_client:get_bucket(?BUCKET, Client).

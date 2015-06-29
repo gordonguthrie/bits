909df22896b966f093dfbe08dce76e1e09b239f6
Gordon Guthrie <gguthrie@basho.com>
2015-06-29 08:55:22 +0000

    update

--- src/gg.erl
+++ src/gg.erl
@@ -15,15 +15,16 @@ format(X, Y) -> Str = io_lib:format(X, Y),
 		log(Str).
 
 format(false, _X, _Y) -> ok;
-format(true,  X,   Y) -> format(X, Y). 
+format(true,  X,   Y) -> format(X, Y).
 
 log(Str) ->
     _FileName = filelib:ensure_dir(?FILENAME),
+    [{registered_name, RN}] = erlang:process_info(self(), [registered_name]),
     case file:open(?FILENAME, [append]) of
-	{ok, Id} -> io:fwrite(Id, "~s~n", [Str]),
-		    file:close(Id);
-	Err      -> {error, Err}
+	{ok, Id} ->
+	    io:fwrite(Id, "On ~p as {~p, ~p}~n- ~s~n",
+		      [node(), self(), RN, Str]),
+	    file:close(Id);
+	Err  ->
+	    {error, Err}
     end.
-	    
-	    
-    

db36a11c32da5cf740a4747fdc9f13a666b1232a
Gordon Guthrie <gguthrie@basho.com>
2015-08-07 08:11:27 +0000

    couple new functions for pping stacktraces and (trying) to pring Funs

--- src/bits.erl
+++ src/bits.erl
@@ -1,7 +1,9 @@
 -module(bits).
 
 -export([
-	log_terms/1
+	 print_fn/2,
+	 log_terms/1,
+	 pp_stacktrace/1
        ]).
 
 -define(LOGFILE, "/tmp/bits.log").
@@ -19,3 +21,21 @@ log_t2(Terms, File) ->
         _ ->
             error
     end.
+
+pp_stacktrace(String) when is_list(String) ->
+    Stacktrace = re:replace(lists:flatten(io_lib:format("~p", [erlang:get_stacktrace()])), "\n", "~n", [global, {return, list}]),
+    io:format(user, "~nStacktrace: " ++ String ++ "~n" ++ Stacktrace ++ "~n", []),
+    ok.
+
+print_fn(String, Fn) when is_list(String) andalso is_function (Fn) ->
+    {env, Env} = erlang:fun_info(Fn, env),
+    io:format("Env is ~p~n", [Env]),
+    % [ABS | _Rest] = lists:reverse(Env),
+    AST = erl_syntax:abstract(Env),
+    io:format("AST is ~p~n", [AST]),
+    Snippet = erl_syntax:revert(AST),
+    io:format("Snippet is ~p~n", [Snippet]),
+    Src = erl_prettypr:format(Snippet),
+    io:format("Src is ~p~n", [Src]),
+    Src2 = re:replace(Src, "\n", "~n", [global, {return, list}]),
+    io:format("printing out Fun: " ++ String ++ "~n~p~n", [Src2]).
